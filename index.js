eaa79fc94e6fe680a68b5033d122033bc5204d72
HeribertoGM <HeribertoGM1999@outlook.com>
2021-01-26 23:15:01 -0600

    setup finished

--- index.js
+++ index.js
@@ -4,8 +4,28 @@ const path = require('path');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('HW!');
+	var options = {
+		root: path.join(__dirname, 'static'),
+		dotfiles: 'deny',
+		headers: {
+			'x-timestamp': Date.now(),
+			'x-sent': true
+		}
+	}
+
+	let fileName = 'temp.html';
+
+	res.status(200).sendFile(fileName, options, (err) => {
+		if(err){
+			console.log(err);
+		}
+		else{
+			console.log("sent: ", fileName);
+		}
+	});
 });
+
+
 
 app.listen(3000, (err) => {
 	if(err){
