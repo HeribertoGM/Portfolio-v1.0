db51b0c1c13e3135d65f1a7227fd666e91599e46
HeribertoGM <HeribertoGM1999@outlook.com>
2021-01-27 18:21:52 -0600

    agrega pagina landing v1

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
 		}
 	}
 
-	let fileName = 'temp.html';
+	let fileName = 'landing.html';
 
 	res.status(200).sendFile(fileName, options, (err) => {
 		if(err){
@@ -25,7 +25,49 @@ app.get('/', (req, res) => {
 	});
 });
 
+app.get('/static/images/:fileName', (req, res) => {
+	var options = {
+		root: path.join(__dirname, 'static', 'images'),
+		dotfiles: 'deny',
+		headers: {
+			'x-timestamp': Date.now(),
+			'x-sent': true
+		}
+	}
 
+	let fileName = req.params.fileName;
+
+	res.status(200).sendFile(fileName, options, (err) => {
+		if(err){
+			console.log(err);
+		}
+		else{
+			console.log("sent: ", fileName);
+		}
+	});
+});
+
+app.get('/static/:fileName', (req, res) => {
+	var options = {
+		root: path.join(__dirname, 'static'),
+		dotfiles: 'deny',
+		headers: {
+			'x-timestamp': Date.now(),
+			'x-sent': true
+		}
+	}
+
+	let fileName = req.params.fileName;
+
+	res.status(200).sendFile(fileName, options, (err) => {
+		if(err){
+			console.log(err);
+		}
+		else{
+			console.log("sent: ", fileName);
+		}
+	});
+});
 
 app.listen(3000, (err) => {
 	if(err){
