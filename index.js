96624d32c202ed9a06e2cf3e5b5ca4d76fca2f0d
hgm <HeribertoGM1999@outlook.com>
2022-09-16 22:23:12 -0500

    UPDATE resume, fix stuff, final commit

--- index.js
+++ index.js
@@ -1,89 +1,82 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const ejs = require('ejs');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const ejs = require("ejs");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const esp = require('./routes/esp');
-const eng = require('./routes/eng');
-const jap = require('./routes/jap');
+const esp = require("./routes/esp");
+const eng = require("./routes/eng");
+const jap = require("./routes/jap");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use('/', esp);
-app.use('/en', eng);
-app.use('/jp', jap);
+app.use("/", esp);
+app.use("/en", eng);
+app.use("/jp", jap);
 
 /*
-* statics imagenes de proyecto
-*/
-app.get('/static/:type/:dir/:fileName', (req, res) => {
+ * statics imagenes de proyecto
+ */
+app.get("/static/:type/:dir/:fileName", (req, res) => {
 	let type = req.params.type;
 	let dir = req.params.dir;
 	let fileName = req.params.fileName;
 
 	var options = {
-		root: path.join(__dirname, 'static', type, dir),
-		dotfiles: 'deny',
+		root: path.join(__dirname, "static", type, dir),
+		dotfiles: "deny",
 		headers: {
-			'x-timestamp': Date.now(),
-			'x-sent': true
-		}
-	}
+			"x-timestamp": Date.now(),
+			"x-sent": true,
+		},
+	};
 
 	res.status(200).sendFile(fileName, options, (err) => {
-		if(err){
+		if (err) {
 			console.log(err);
-		}
-		else{
+		} else {
 			console.log("sent: ", fileName);
 		}
 	});
 });
 
 /*
-*	statics general
-*/
-app.get('/static/:type/:fileName', (req, res) => {
+ *	statics general
+ */
+app.get("/static/:type/:fileName", (req, res) => {
 	let type = req.params.type;
 	let fileName = req.params.fileName;
 
 	var options = {
-		root: path.join(__dirname, 'static', type),
-		dotfiles: 'deny',
+		root: path.join(__dirname, "static", type),
+		dotfiles: "deny",
 		headers: {
-			'x-timestamp': Date.now(),
-			'x-sent': true
-		}
-	}
+			"x-timestamp": Date.now(),
+			"x-sent": true,
+		},
+	};
 
 	res.status(200).sendFile(fileName, options, (err) => {
-		if(err){
+		if (err) {
 			console.log(err);
-		}
-		else{
+		} else {
 			console.log("sent: ", fileName);
 		}
 	});
 });
 
 /*
-*	listen
-*/
-app.listen(port, '192.23.65.14', (err) => {
-	if(err){
+ *	listen
+ */
+app.listen(port, (err) => {
+	if (err) {
 		console.log(err);
-	}
-	else{
-		console.log('listening on 192.23.65.14:'+port);
+	} else {
+		console.log("listening on localhost:" + port);
 	}
 });
-
-
-
-
